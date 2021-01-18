d9e7a4031da622b4f3e65f9c0614d0d755a31f7b
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-01-18 15:03:29 +0700

    Revert "initial commit"
    This reverts commit 600b833c264b87c16e22f40dfd5b2177fd8fb3f5.

--- app/src/main/java/com/husnul23/githubsubmission/Github.kt
+++
@@ -1,7 +0,0 @@
-package com.husnul23.githubsubmission
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class Github (var username: String = "", var avatar: String = "", ) : Parcelable
-
